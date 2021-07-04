fa2fff4368d2fb4cf6e69dbf63895fc8b4ef0f59
Sagar Kanojia <66565766+hyperassassin@users.noreply.github.com>
2021-07-04 19:58:12 +0530

    Add files via upload

---
+++ breadth_first_search.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+    
+    def addedge(self,u,v):
+        self.graph[u].append(v)
+    
+    def bfs(self,s):
+        visited = [False] * (max(self.graph) + 1)
+        queue = []
+        queue.append(s)
+        visited[s] = True
+        while queue:
+            s = queue.pop(0)
+            print(s , end = " ")
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+#Implementation
+g = Graph()
+g.addedge(0,1)
+g.addedge(0,2)
+g.addedge(1,2)
+g.addedge(2,0)
+g.addedge(2,3)
+g.addedge(3,3)
+
+print("Breadth First Search (BFS) starting from vertex 2")
+g.bfs(2)
+
